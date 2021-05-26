f671c5ded74dd7c1b6d985eb937fb13c6b4de745
nakamura <gkii.8r@gmail.com>
2021-05-27 03:32:58 +0900

    update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,17 @@
+<?php
+
+// var_dump($posts);
+// exit;
+dd($post);
+
+?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My BBS</title>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
     <div class="container">
         <h1>My BBS</h1>
