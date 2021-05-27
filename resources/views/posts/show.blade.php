884f7eed482ace5e956024fd4704e857207cfb19
nakamura <gkii.8r@gmail.com>
2021-05-27 13:39:59 +0900

    create show.blade.php

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>My BBS</h1>
+        <ul>
+            @forelse ($posts as $index => $post)
+                <li>
+                    <a href="/posts/{{ $index }}">
+                        {{ $post }}
+                    </a>
+                </li>
+            @empty
+                <li>No posts yet!</li>
+            @endforelse
+        </ul>
+    </div>
+</body>
+</html>
