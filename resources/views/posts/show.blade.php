4b03630a82c90a5d50c2ba56ffd0bf0d93e53519
nakamura <gkii.8r@gmail.com>
2021-05-27 16:35:46 +0900

    localhostのurlでcssの位置を指定

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title>My BBS</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+    {{-- public フォルダからのパスを使って URL を指定する --}}
 </head>
 <body>
     <div class="container">
