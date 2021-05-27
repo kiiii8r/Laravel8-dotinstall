d91180b32cf09bacbf4b0d7cdc1d921f8c4f3b50
nakamura <gkii.8r@gmail.com>
2021-05-27 12:48:20 +0900

    blade機能検証

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,3 @@
-<?php
-
-// var_dump($posts);
-// exit;
-dd($post);
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -15,10 +8,22 @@ dd($post);
 <body>
     <div class="container">
         <h1>My BBS</h1>
+        {{-- <ul> --}}
+            {{-- <li><?php echo htmlspecialchars($posts[0], ENT_QUOTES, 'UTF-8'); ?></li> --}}
+            {{-- <li>{{ $posts[0] }}</li>
+            <li>{{ $posts[1] }}</li>
+            <li>{{ $posts[2] }}</li> --}}
+        {{-- </ul> --}}
         <ul>
-            <li>Title</li>
-            <li>Title</li>
-            <li>Title</li>
+            {{-- @foreach ($posts as $post)
+                <li>{{ $post }}</li>
+            @endforeach --}}
+
+            @forelse ($posts as $post)
+                <li>{{ $post }}</li>
+            @empty
+                <li>No posts yet!</li>
+            @endforelse
         </ul>
     </div>
 </body>
