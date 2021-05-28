48f4144fc5e4c22f13387af915e2392e80db88e0
nakamura <gkii.8r@gmail.com>
2021-05-28 14:50:34 +0900

    add x-layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-    {{-- public フォルダからのパスを使って URL を指定する --}}
-</head>
-<body>
-    <div class="container">
-        <div class="back-link">
-            &laquo; <a href="{{ route('posts.index') }}">Back</a>
-        </div>
+<x-layout>
+    <x-slot name="title">
+        {{ $post }} - My BBS
+    </x-slot>
 
-        <h1>{{ $post }}</h1>
+    <div class="back-link">
+        &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
-</body>
-</html>
+
+    <h1>{{ $post }}</h1>
+</x-layout>
